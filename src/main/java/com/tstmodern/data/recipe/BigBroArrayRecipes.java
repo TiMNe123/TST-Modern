433125bc64327367d57496381c221fd7bc279404
TiMNe123 <mtien10062002@gmail.com>
2026-08-27 13:00:32 +0700

    feat: restore BigBroArray construction recipe progression

--- src/main/java/com/tstmodern/data/recipe/BigBroArrayRecipes.java
+++ src/main/java/com/tstmodern/data/recipe/BigBroArrayRecipes.java
@@ -1,9 +1,7 @@
 package com.tstmodern.data.recipe;
 
 import static com.gregtechceu.gtceu.api.GTValues.*;
-import static com.gregtechceu.gtceu.api.data.tag.TagPrefix.frameGt;
 import static com.gregtechceu.gtceu.api.data.tag.TagPrefix.plateDense;
-import static com.gregtechceu.gtceu.api.data.tag.TagPrefix.wireGtDouble;
 import static com.gregtechceu.gtceu.common.data.GTRecipeTypes.ASSEMBLER_RECIPES;
 import static com.gregtechceu.gtceu.common.data.GTRecipeTypes.ASSEMBLY_LINE_RECIPES;
 
@@ -11,9 +9,7 @@ import java.util.function.Consumer;
 
 import com.gregtechceu.gtceu.common.data.GTBlocks;
 import com.gregtechceu.gtceu.common.data.GTItems;
-import com.gregtechceu.gtceu.common.data.GTMachines;
 import com.gregtechceu.gtceu.common.data.GTMaterials;
-import com.gregtechceu.gtceu.data.recipe.CustomTags;
 import com.tstmodern.registry.TSTBlocks;
 import com.tstmodern.registry.machine.BigBroArrayDefinition;
 
@@ -28,82 +24,126 @@ public final class BigBroArrayRecipes {
     private BigBroArrayRecipes() {}
 
     public static void register(Consumer<FinishedRecipe> provider) {
-        // Parallel Casing MK1 (HV-tier components)
-        ASSEMBLER_RECIPES.recipeBuilder("parallel_casing_mk1")
-                .inputItems(GTBlocks.CASING_STAINLESS_CLEAN.asStack())
-                .inputItems(CustomTags.HV_CIRCUITS, 2)
-                .inputItems(GTItems.ROBOT_ARM_HV.asStack(2))
-                .inputItems(wireGtDouble, GTMaterials.Electrum, 4)
-                .inputFluids(GTMaterials.SolderingAlloy.getFluid(L * 2))
-                .outputItems(new ItemStack(TSTBlocks.PARALLEL_CASING_MK1.get()))
-                .duration(200).EUt(VA[HV])
-                .save(provider);
-
-        // Parallel Casing MK2 (EV-tier components)
-        ASSEMBLER_RECIPES.recipeBuilder("parallel_casing_mk2")
-                .inputItems(new ItemStack(TSTBlocks.PARALLEL_CASING_MK1.get()))
-                .inputItems(CustomTags.EV_CIRCUITS, 2)
-                .inputItems(GTItems.ROBOT_ARM_EV.asStack(2))
-                .inputItems(wireGtDouble, GTMaterials.Aluminium, 4)
-                .inputFluids(GTMaterials.SolderingAlloy.getFluid(L * 4))
-                .outputItems(new ItemStack(TSTBlocks.PARALLEL_CASING_MK2.get()))
-                .duration(300).EUt(VA[EV])
-                .save(provider);
-
-        // Parallel Casing MK3 (IV-tier components)
-        ASSEMBLER_RECIPES.recipeBuilder("parallel_casing_mk3")
-                .inputItems(new ItemStack(TSTBlocks.PARALLEL_CASING_MK2.get()))
-                .inputItems(CustomTags.IV_CIRCUITS, 2)
-                .inputItems(GTItems.ROBOT_ARM_IV.asStack(2))
-                .inputItems(wireGtDouble, GTMaterials.Platinum, 4)
-                .inputFluids(GTMaterials.SolderingAlloy.getFluid(L * 6))
-                .outputItems(new ItemStack(TSTBlocks.PARALLEL_CASING_MK3.get()))
-                .duration(400).EUt(VA[IV])
-                .save(provider);
-
-        // Parallel Casing MK4 (LuV-tier components)
-        ASSEMBLER_RECIPES.recipeBuilder("parallel_casing_mk4")
-                .inputItems(new ItemStack(TSTBlocks.PARALLEL_CASING_MK3.get()))
-                .inputItems(CustomTags.LuV_CIRCUITS, 2)
-                .inputItems(GTItems.ROBOT_ARM_LuV.asStack(2))
-                .inputItems(wireGtDouble, GTMaterials.NiobiumTitanium, 4)
-                .inputFluids(GTMaterials.SolderingAlloy.getFluid(L * 8))
-                .outputItems(new ItemStack(TSTBlocks.PARALLEL_CASING_MK4.get()))
-                .duration(500).EUt(VA[LuV])
-                .save(provider);
-
-        // Parallel Casing MK5 (ZPM-tier components)
-        ASSEMBLER_RECIPES.recipeBuilder("parallel_casing_mk5")
-                .inputItems(new ItemStack(TSTBlocks.PARALLEL_CASING_MK4.get()))
-                .inputItems(CustomTags.ZPM_CIRCUITS, 2)
-                .inputItems(GTItems.ROBOT_ARM_ZPM.asStack(2))
-                .inputItems(wireGtDouble, GTMaterials.VanadiumGallium, 4)
-                .inputFluids(GTMaterials.SolderingAlloy.getFluid(L * 10))
-                .outputItems(new ItemStack(TSTBlocks.PARALLEL_CASING_MK5.get()))
-                .duration(600).EUt(VA[ZPM])
-                .save(provider);
-
-        // BigBroArray Controller Assembly Line Recipe (UV-tier)
-        ASSEMBLY_LINE_RECIPES.recipeBuilder("big_bro_array")
-                .inputItems(GTBlocks.CASING_TUNGSTENSTEEL_ROBUST.asStack(16))
-                .inputItems(CustomTags.UV_CIRCUITS, 4)
-                .inputItems(GTItems.ROBOT_ARM_UV.asStack(4))
-                .inputItems(GTItems.CONVEYOR_MODULE_UV.asStack(4))
-                .inputItems(GTItems.ELECTRIC_PUMP_UV.asStack(4))
-                .inputItems(GTItems.FIELD_GENERATOR_UV.asStack(2))
-                .inputItems(GTItems.EMITTER_UV.asStack(4))
-                .inputItems(GTItems.SENSOR_UV.asStack(4))
-                .inputItems(frameGt, GTMaterials.Tritanium, 4)
-                .inputItems(plateDense, GTMaterials.TungstenSteel, 8)
-                .inputFluids(GTMaterials.SolderingAlloy.getFluid(L * 32))
-                .inputFluids(GTMaterials.Lubricant.getFluid(4000))
+        // BigBroArray Controller (IV Assembler, 24000 ticks, 6400 EU/t)
+        ASSEMBLER_RECIPES.recipeBuilder("big_bro_array")
+                .inputItems(GTItems.TOOL_DATA_ORB.asStack(16))
+                .inputItems(GTItems.ROBOT_ARM_IV.asStack(8))
+                .inputItems(GTItems.ELECTRIC_PISTON_IV.asStack(8))
+                .inputItems(GTItems.ELECTRIC_MOTOR_IV.asStack(8))
+                .inputItems(GTItems.ELECTRIC_PUMP_IV.asStack(8))
+                .inputItems(GTItems.FIELD_GENERATOR_IV.asStack(4))
+                .inputItems(GTItems.EMITTER_IV.asStack(4))
+                .inputItems(GTItems.SENSOR_IV.asStack(4))
+                .inputFluids(GTMaterials.Titanium.getFluid(2880))
                 .outputItems(BigBroArrayDefinition.MACHINE.asStack())
-                .duration(1200)
-                .EUt(VA[UV])
+                .duration(24000)
+                .EUt(6400)
+                .save(provider);
+
+        // Parallel Casing MK1 (IV Assembler, 3000 ticks, 6400 EU/t)
+        ASSEMBLER_RECIPES.recipeBuilder("parallel_casing_mk1")
+                .inputItems(GTBlocks.CASING_TUNGSTENSTEEL_ROBUST.asStack())
+                .inputItems(GTItems.ROBOT_ARM_IV.asStack(2))
+                .inputItems(GTItems.ELECTRIC_PISTON_IV.asStack(2))
+                .inputItems(GTItems.ELECTRIC_MOTOR_IV.asStack(2))
+                .inputItems(GTItems.ELECTRIC_PUMP_IV.asStack(2))
+                .inputItems(GTItems.FIELD_GENERATOR_IV.asStack(1))
+                .inputItems(GTItems.EMITTER_IV.asStack(1))
+                .inputItems(GTItems.SENSOR_IV.asStack(1))
+                .inputFluids(GTMaterials.Titanium.getFluid(1152))
+                .outputItems(new ItemStack(TSTBlocks.PARALLEL_CASING_MK1.get()))
+                .duration(3000)
+                .EUt(6400)
+                .save(provider);
+
+        // Parallel Casing MK2 (ZPM Assembly Line, 12000 ticks, 100000 EU/t)
+        ASSEMBLY_LINE_RECIPES.recipeBuilder("parallel_casing_mk2")
+                .inputItems(new ItemStack(TSTBlocks.PARALLEL_CASING_MK1.get()))
+                .inputItems(GTItems.FIELD_GENERATOR_ZPM.asStack(4))
+                .inputItems(GTItems.ENERGY_CLUSTER.asStack(4))
+                .inputItems(GTItems.SENSOR_ZPM.asStack(4))
+                .inputItems(GTItems.EMITTER_ZPM.asStack(4))
+                .inputItems(GTItems.ELECTRIC_MOTOR_ZPM.asStack(4))
+                .inputItems(GTItems.ROBOT_ARM_ZPM.asStack(4))
+                .inputItems(GTItems.ELECTRIC_PISTON_ZPM.asStack(4))
+                .inputItems(GTItems.ELECTRIC_PUMP_ZPM.asStack(4))
+                .inputItems(plateDense, GTMaterials.Lead, 8)
+                .inputFluids(GTMaterials.SolderingAlloy.getFluid(5760))
+                .inputFluids(GTMaterials.Osmiridium.getFluid(5760))
+                .outputItems(new ItemStack(TSTBlocks.PARALLEL_CASING_MK2.get()))
+                .duration(12000)
+                .EUt(100000)
+                .scannerResearch(b -> b
+                        .researchStack(new ItemStack(TSTBlocks.PARALLEL_CASING_MK1.get()))
+                        .duration(288000)
+                        .EUt(32))
+                .save(provider);
+
+        // Parallel Casing MK3 (UHV Assembly Line, 24000 ticks, 2000000 EU/t)
+        ASSEMBLY_LINE_RECIPES.recipeBuilder("parallel_casing_mk3")
+                .inputItems(new ItemStack(TSTBlocks.PARALLEL_CASING_MK2.get()))
+                .inputItems(GTItems.FIELD_GENERATOR_UHV.asStack(4))
+                .inputItems(GTItems.ENERGY_CLUSTER.asStack(4))
+                .inputItems(GTItems.SENSOR_UHV.asStack(4))
+                .inputItems(GTItems.EMITTER_UHV.asStack(4))
+                .inputItems(GTItems.ELECTRIC_MOTOR_UHV.asStack(4))
+                .inputItems(GTItems.ROBOT_ARM_UHV.asStack(4))
+                .inputItems(GTItems.ELECTRIC_PISTON_UHV.asStack(4))
+                .inputItems(GTItems.ELECTRIC_PUMP_UHV.asStack(4))
+                .inputItems(plateDense, GTMaterials.Europium, 8)
+                .inputFluids(GTMaterials.SolderingAlloy.getFluid(5760))
+                .inputFluids(GTMaterials.Tritanium.getFluid(5760))
+                .outputItems(new ItemStack(TSTBlocks.PARALLEL_CASING_MK3.get()))
+                .duration(24000)
+                .EUt(2000000)
+                .scannerResearch(b -> b
+                        .researchStack(new ItemStack(TSTBlocks.PARALLEL_CASING_MK2.get()))
+                        .duration(576000)
+                        .EUt(32))
+                .save(provider);
+
+        // Parallel Casing MK4 (UIV Researchable Assembly Line, 24000 ticks, 8000000 EU/t)
+        ASSEMBLY_LINE_RECIPES.recipeBuilder("parallel_casing_mk4")
+                .inputItems(new ItemStack(TSTBlocks.PARALLEL_CASING_MK3.get(), 16))
+                .inputItems(GTItems.FIELD_GENERATOR_UIV.asStack(8))
+                .inputItems(GTItems.SENSOR_UIV.asStack(8))
+                .inputItems(GTItems.EMITTER_UIV.asStack(8))
+                .inputItems(GTItems.ELECTRIC_MOTOR_UIV.asStack(8))
+                .inputItems(GTItems.ROBOT_ARM_UIV.asStack(8))
+                .inputItems(GTItems.ELECTRIC_PISTON_UIV.asStack(8))
+                .inputItems(GTItems.ELECTRIC_PUMP_UIV.asStack(8))
+                .inputItems(plateDense, GTMaterials.Tritanium, 16)
+                .inputFluids(GTMaterials.SolderingAlloy.getFluid(11520))
+                .inputFluids(GTMaterials.Naquadria.getFluid(11520))
+                .outputItems(new ItemStack(TSTBlocks.PARALLEL_CASING_MK4.get()))
+                .duration(24000)
+                .EUt(8000000)
                 .stationResearch(b -> b
-                        .researchStack(GTMachines.HULL[UV].asStack())
+                        .researchStack(new ItemStack(TSTBlocks.PARALLEL_CASING_MK3.get()))
                         .CWUt(64, 4800)
-                        .EUt(VA[UV]))
+                        .EUt(8000000))
+                .save(provider);
+
+        // Parallel Casing MK5 (UXV Researchable Assembly Line, 24000 ticks, 32000000 EU/t)
+        ASSEMBLY_LINE_RECIPES.recipeBuilder("parallel_casing_mk5")
+                .inputItems(new ItemStack(TSTBlocks.PARALLEL_CASING_MK4.get(), 16))
+                .inputItems(GTItems.FIELD_GENERATOR_UXV.asStack(16))
+                .inputItems(GTItems.SENSOR_UXV.asStack(16))
+                .inputItems(GTItems.EMITTER_UXV.asStack(16))
+                .inputItems(GTItems.ELECTRIC_MOTOR_UXV.asStack(16))
+                .inputItems(GTItems.ROBOT_ARM_UXV.asStack(16))
+                .inputItems(GTItems.ELECTRIC_PISTON_UXV.asStack(16))
+                .inputItems(GTItems.ELECTRIC_PUMP_UXV.asStack(16))
+                .inputItems(plateDense, GTMaterials.Neutronium, 16)
+                .inputFluids(GTMaterials.SolderingAlloy.getFluid(23040))
+                .inputFluids(GTMaterials.Neutronium.getFluid(23040))
+                .outputItems(new ItemStack(TSTBlocks.PARALLEL_CASING_MK5.get()))
+                .duration(24000)
+                .EUt(32000000)
+                .stationResearch(b -> b
+                        .researchStack(new ItemStack(TSTBlocks.PARALLEL_CASING_MK4.get()))
+                        .CWUt(128, 9600)
+                        .EUt(32000000))
                 .save(provider);
     }
 }
